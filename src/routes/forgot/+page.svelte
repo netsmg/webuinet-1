db8f6940e471117b410a0f9e90cbd50188274abc
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-03-30 04:42:54 +0600

    Create +page.svelte

---
+++ src/routes/forgot/+page.svelte
@@ -0,0 +1,42 @@
+<svelte:head>
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/blueimp-gallery.min.css">
+	<link rel="stylesheet" href="/css/splide.min.css">
+	<link rel="stylesheet" href="/css/slimselect.css">
+	<link rel="stylesheet" href="/css/main.css">
+</svelte:head>
+<!-- page wrap -->
+	<div class="section section--content">
+		<div class="section__content">
+			<!-- form -->
+			<form action="#" class="form form--content">
+				<div class="form__logo-wrap">
+					<a href="index.html" class="form__logo">
+						<img src="img/logo.svg" alt="">
+					</a>
+					<span class="form__tagline">Play to earn <br>HTML Template</span>
+				</div>
+
+				<div class="form__group">
+					<input type="text" class="form__input" placeholder="Email">
+				</div>
+
+				<div class="form__group form__group--checkbox">
+					<input id="remember" name="remember" type="checkbox">
+					<label for="remember">I agree to the <a href="privacy.html">Privacy Policy</a></label>
+				</div>
+				
+				<button class="form__btn" type="button">Send</button>
+
+				<span class="form__text form__text--center">We will send a password to your Email</span>
+			</form>
+			<!-- end form -->
+		</div>
+
+		<!-- bg animation -->
+		<div id="canvas2" class="section__canvas"></div>
+		<!-- end bg animation -->
+	</div>
+	<!-- end page wrap -->
+
+	<!-- JS -->
