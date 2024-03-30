7e9beb84a413bccf5dc6baa91f6bdf2848403d0c
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-03-30 18:12:20 +0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,10 @@
 					<!-- hero content -->
 					<div class="hero__content">
 						<span class="hero__tagline">WELCOME</span>
-						<h2 class="hero__title">Play to Earn Ecosystem</h2>
+						<h2 class="hero__title">Let&apos;s build together</h2>
 						<p class="hero__text">Metaverse Aggregator & Web 3.0 Launchpad</p>
 						<div class="hero__btns">
-							<a href="aggregator" class="hero__btn"><span>Explore</span></a>
+							<a href="explore" class="hero__btn"><span>Explore</span></a>
 							<a href="add" class="hero__btn hero__btn--nephrite"><span>Apply</span></a>
 						</div>
 
@@ -92,103 +92,7 @@
 	</section>
 	<!-- end tokenomics -->
 
-	<!-- roadmap -->
-	<section class="section section--light">
-		<div class="container">
-			<div class="row">
-				<!-- section title -->
-				<div class="col-12">
-					<div class="section__title section__title--carousel">
-						<h2>Roadmap</h2>
-					</div>
-				</div>
-				<!-- end section title -->
-
-				<!-- carousel -->
-				<div class="col-12">
-					<div class="section__carousel section__carousel--block splide splide--block">
-						<div class="splide__arrows">
-							<button class="splide__arrow splide__arrow--prev" type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17,11H9.41l3.3-3.29a1,1,0,1,0-1.42-1.42l-5,5a1,1,0,0,0-.21.33,1,1,0,0,0,0,.76,1,1,0,0,0,.21.33l5,5a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42L9.41,13H17a1,1,0,0,0,0-2Z"></path></svg></button>
-							<button class="splide__arrow splide__arrow--next" type="button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M17.92,11.62a1,1,0,0,0-.21-.33l-5-5a1,1,0,0,0-1.42,1.42L14.59,11H7a1,1,0,0,0,0,2h7.59l-3.3,3.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l5-5a1,1,0,0,0,.21-.33A1,1,0,0,0,17.92,11.62Z"></path></svg></button>
-						</div>
-
-						<div class="splide__track">
-							<ul class="splide__list">
-								<li class="splide__slide">
-									<div class="roadmap roadmap--active">
-										<h3 class="roadmap__title">2023 Q4</h3>
-										<ul class="roadmap__list">
-											<li>Project start</li>
-											<li>Tiers system</li>
-											<li>Tokenomics</li>
-											<li>Seed round</li>
-											<li>Aggregator – MVP</li>
-										</ul>
-									</div>
-								</li>
-
-								<li class="splide__slide">
-									<div class="roadmap roadmap--active">
-										<h3 class="roadmap__title">2024 Q1</h3>
-										<ul class="roadmap__list">
-											<li>Aggregator – Alpha</li>
-											<li>MVP Launchpad</li>
-											<li>Private round</li>
-											<li>Strategic round</li>
-											<li>GameFi ratings</li>
-										</ul>
-									</div>
-								</li>
-
-								<li class="splide__slide">
-									<div class="roadmap">
-										<h3 class="roadmap__title">2024 Q2</h3>
-										<ul class="roadmap__list">
-											<li>Aggregator – Beta</li>
-											<li>Smart contract audit</li>
-											<li>KYC integration</li>
-											<li>Launchpad – Beta</li>
-											<li>Public round</li>
-										</ul>
-									</div>
-								</li>
-
-								<li class="splide__slide">
-									<div class="roadmap">
-										<h3 class="roadmap__title">2024 Q3</h3>
-										<ul class="roadmap__list">
-											<li>CEX listing</li>
-											<li>Staking pools</li>
-											<li>MVP guild aggregator</li>
-											<li>MVP NFT pawnshop</li>
-											<li>Referral program</li>
-										</ul>
-									</div>
-								</li>
-
-								<li class="splide__slide">
-									<div class="roadmap">
-										<h3 class="roadmap__title">2024 Q4</h3>
-										<ul class="roadmap__list">
-											<li>Karma holders</li>
-											<li>DAO governance launch</li>
-											<li>NFT pawnshop launch</li>
-											<li>Guild aggregator launch</li>
-											<li>Rewards and challenges</li>
-										</ul>
-									</div>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<!-- end carousel -->
-			</div>
-		</div>
-
-		<div id="canvas2" class="section__canvas"></div>
-	</section>
-	<!-- end roadmap -->
+	
 
 	<!-- resources -->
 	<section class="section">
@@ -244,97 +148,6 @@
 	</section>
 	<!-- end resources -->
 
-	<!-- sponsors -->
-	<section class="section section--light">
-		<div class="container">
-			<div class="row">
-				<!-- section title -->
-				<div class="col-12">
-					<div class="section__title section__title--between">
-						<h2>Some Of Our <br>Awesome Partners</h2>
-						<p>We take pride in collaborating with our partners who help us provide the best services to our clients. If you'd like to become our partner, please <br><a href="contacts/">contact us.</a></p>
-					</div>
-				</div>
-				<!-- end section title -->
-
-				<!-- sponsors -->
-				<div class="col-12">
-					<ul class="sponsors">
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo1.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo2.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo3.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo4.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo5.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo6.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo7.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo8.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo2.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo3.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo1.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-						<li>
-							<a href="/">
-								<img src="img/sponsors/logo6.svg" alt="">
-								<span>thesponsor.com</span>
-							</a>
-						</li>
-					</ul>
-				</div>
-				<!-- end sponsors -->
 			</div>
 		</div>
 	</section>
