1742a0e792a04c563cad882d2ed7cac6218c5827
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-03-30 22:19:36 +0600

    Create +page.svelte

---
+++ src/routes/download/+page.svelte
@@ -0,0 +1,18 @@
+<!-- DownloadButton.svelte -->
+<script>
+  import { saveAs } from 'file-saver';
+
+  const downloadFile = () => {
+    // Replace 'url_to_your_file' with the actual URL of the file you want to download
+    const fileUrl = 'https://webuinet.vercel.app/logo.svg';
+    // Replace 'file_name.extension' with the desired name and extension of the file
+    const fileName = 'logo.svg';
+
+    fetch(fileUrl)
+      .then(response => response.blob())
+      .then(blob => saveAs(blob, fileName))
+      .catch(error => console.error('Error downloading file:', error));
+  };
+</script>
+
+<button on:click={downloadFile}>Download File</button>
