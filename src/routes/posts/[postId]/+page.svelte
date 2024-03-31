1ee7070ad597c3c4b23d5a187dc70441614ae197
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-03-31 21:49:19 +0600

    Create +page.svelte

---
+++ src/routes/posts/[postId]/+page.svelte
@@ -0,0 +1,33 @@
+<!-- PostDetails.svelte -->
+<script>
+  import { onMount } from 'svelte';
+  import { getFirestore, doc, getDoc } from 'firebase/firestore';
+  import { page } from "$app/stores";
+  import { app } from '../../firebase';
+
+  let post = {};
+
+  onMount(async () => {
+    const postId = $page.params.postId;
+    const db = getFirestore(app);
+    const postRef = doc(db, 'blogs', postId);
+    const postSnapshot = await getDoc(postRef);
+    
+    if (postSnapshot.exists()) {
+      post = postSnapshot.data();
+    } else {
+      console.error('Post not found');
+    }
+  });
+</script>
+
+<div>
+  {#if Object.keys(post).length !== 0}
+    <h1>{post.title}</h1>
+    <img src="{post.image}" alt="Post Image">
+    <p>{post.content}</p>
+    <!-- Add more details here -->
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</div>
