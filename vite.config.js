7f3e3cb9cb461ce2c2c9fa525622be492140157a
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-04-02 19:53:42 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [
     svelte({
-      /* plugin options */
-	    plugins: [sveltekit()]
+      
     })
   ]
 });
