ebaee6c08cfbb299b15733864c02e4ccc81301eb
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-04-02 19:17:16 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,49 @@
 <script>
   import { onMount } from 'svelte';
-
+  import {onAuthStateChanged, signOut} from 'firebase/auth'
+  import toast, { Toaster } from 'svelte-french-toast';
+  import { fauth } from "../firebase";
+  
+  import {userStore} from "../stores/userStore"
+  
   let headerBtn, headerNav, header, myModalEl;
 
   onMount(() => {
+document.title = "WEBUInet | Home";
+   const  saveSettings =() => {
+      return new Promise((resolve, reject) => {
+        let x = setInterval(()=>{
+          if($userStore.loggedIn===true){
+          resolve();
+          clearInterval(x);
+        }else if($userStore.loggedIn===false){
+          reject();
+          clearInterval(x);
+        }
+        }, 100)
+      });
+    }
+    toast.promise(
+        saveSettings(),
+        {
+          loading: 'Checking...',
+          success: 'Signed In!',
+          error: 'You are not signed In!',
+        },
+    );
+    onAuthStateChanged(fauth, async(user) => {
+        if(user) {
+            userStore.set({...user, loggedIn: true});
+            me = user;
+        } else{
+           me = false
+            console.log('User Not Logged In!');
+            userStore.set({loggedIn: false});
+        }
+       });
+  
+  });
+
     headerBtn = document.querySelector('.header__btn');
     headerNav = document.querySelector('.header__nav');
     header = document.querySelector('.header');
@@ -59,6 +99,7 @@
 </svelte:head>
 
 <!-- Your HTML structure goes here -->
+<Toaster/>
 <header class="header">
   <div class="container">
     <div class="row">
